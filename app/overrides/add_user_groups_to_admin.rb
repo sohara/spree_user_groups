22ca9df4d0e0f052b1075b386ad232b38ebeba03
Jonathan Tapia <jonathan.tapia@crowdint.com>
2014-02-21 11:42:33 -0600

    Add support for rails 4 and spree 2-2

--- app/overrides/add_user_groups_to_admin.rb
+++ app/overrides/add_user_groups_to_admin.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                      :name => "add_user_groups_link_configuration_menu",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :text => %q{<%= configurations_sidebar_menu_item t(:user_groups), admin_user_groups_path %>},
+                     :text => %q{<%= configurations_sidebar_menu_item Spree.t(:user_groups), admin_user_groups_path %>},
                      :disabled => false)
 
 Deface::Override.new(:virtual_path => "spree/admin/users/_form",
